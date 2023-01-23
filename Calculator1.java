ea9983c431d80dc4994c7b5976af05baac5d803c
salina bista <salinabista37@gmail.com>
2023-01-22 22:55:31 -0500

    working on add functionality

--- Calculator1.java
+++ Calculator1.java
@@ -1,8 +1,9 @@
 import java.lang.*;
 
 Class Calculator1{
-	
+	public int add(int x,int y){ return x+y; }
 	public static void main(String args[]) {
 		System.out.println("This is Calculator2 feature");
+		
 	}
 }
